2f65a7f2d5d2b49ec0ca81193696966b4914c01d
rami3rouhana <80242717+rami3rouhana@users.noreply.github.com>
2022-09-30 15:26:31 +0300

    refactor string sort

--- app/Http/Controllers/IntroController.php
+++ app/Http/Controllers/IntroController.php
@@ -10,12 +10,59 @@ class IntroController extends Controller
     // Exercice 1 sort the string by order
     function sortString(Request $req)
     {
-        return json_decode($req->getContent(), true)->string;
         try {
-            return response()->json([
-                "Success" => true
-            ]);
+            $string = $req->json()->all()['string'];
+
+            if (preg_match("#^[a-zA-Z0-9]+$#", $string)) {
+
+                $array = str_split($string);
+
+                sort($array, SORT_NATURAL  | SORT_FLAG_CASE);
+
+                $split = [];
+                $count = 0;
+                foreach (array_keys($array) as $key) {
+
+                    if (preg_match("/^[a-zA-Z]+$/", $array[$key])) {
+
+                        if ($key === 0) {
+                            break;
+                        }
+                        
+                        $split = array_chunk($array, $key);
+                        $count++;
+                        break;
+                    }
+                }
+
+                $counter = 0;
+
+                $res = '';
+
+                if ($split !== []) {
+                    foreach ($split as $combine) {
+                        if ($counter++ == 0) continue;
+                        $res = $res . implode($combine);
+                    }
+                    $result = $res . implode($split[0]);
+                } else {
+                    $result = implode($array);
+                }
+
+                return response()->json([
+                    "Success" => true,
+                    "Result" => $result
+                ]);
+
+            } else {
+
+                return response()->json([
+                    "Success" => false,
+                    "Error" => "Contains special characters."
+                ]);
+            }
         } catch (Exception $e) {
+
             return response()->json([
                 "Success" => false,
                 "Error" => "$e"
